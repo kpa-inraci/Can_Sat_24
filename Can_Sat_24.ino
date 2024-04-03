56364089257349d4dab7cd6fa8c643e41393fbf0
laflecherapide <laflecherapide@hotmail.com>
2024-04-03 13:02:03 +0100

    ajout gestion timing regu

--- Can_Sat_24.ino
+++ Can_Sat_24.ino
@@ -2,14 +2,12 @@
 #include "backup_managemet.h"
 extern char sensor_type;
 extern unsigned long Time_ms;  // "temps" en milliseconde depuis le dernier reset du uP
+int compteur_regu = 0;
+int compteur_donne = 0;
 
-//wesh, jesuis figué
-//moi aussi je suis nez avant toi !!!
-//bjr quentin
-
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
+  Serial1.begin(115200);
   delay(2000);
   init_backup_management();
   Wire.begin();
@@ -20,32 +18,44 @@ void setup()
   attachAndWriteServo(Servomoteur2, SERVO_Pin_2, 0);
   init_flash();
   initPinIO(BUZZER_Pin, OUTPUT, LOW);
-  buzzer_toggle(1000); 
+  buzzer_toggle(1000);
   Time_ms = millis();
 }
-char status=0;
-void loop() 
-{
- 
-  status=backup_choice();
-  if (millis() >= Time_ms + 10)
-  { //prends une mesure toute les 10ms
-    get_data();  
-    Packetnum++;
+char status = 0;
+void loop() {
+  Serial1.write(178);
+  status = backup_choice();
+  if (millis() >= Time_ms + 10) {  //prends une mesure toute les 10ms
+    get_data();
+    Time_ms = millis();
+    compteur_regu++;
+    compteur_donne++;
   }
- 
-  if (millis() >= Time_ms + 1000) //envois les données toutes les secondes
-  {
-    Time_ms = millis();     
+  if (compteur_regu >= 10) {
+    compteur_regu = 0;
+    //regulation
 
-    switch (status)
-    {
-      case VERTICAN_format_file:  formatMemory(); Packetnum = 0;		break;
-      case VERTICAN_extract_file: extractData(); Serial.println("data has been extracted enter something to run");while(1){if (Serial.available()){break;}}	  break;
-      case VERTICAN_run:          send_all_data();	break;
-      
-    }
+  }
+
+
+  switch (status) {
+    case VERTICAN_format_file:
+      formatMemory();
+      Packetnum = 0;
+      break;
+    case VERTICAN_extract_file:
+      extractData();
+      Serial.println("data has been extracted enter something to run");
+      while (1) {
+        if (Serial.available()) { break; }
+      }
+      break;
+    case VERTICAN_run:
+      if (compteur_donne >= 100) {
+        Packetnum++;
+        send_all_data();
+        compteur_donne = 0;
+      }
+      break;
   }
 }
-
-
